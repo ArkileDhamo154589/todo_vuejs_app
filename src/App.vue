e160ae5b7873942b06ae82fd3d0e840bec209183
Arqile Dhamo <arkiledamo97@gmail.com>
2023-09-04 19:46:46 +0300

    Input show the name
    In this code , when  you put a name inside to the placeholder , if you refresh the page you will see the name in the input. Also add an empty function addTodo and a form

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,42 @@
 <script setup>
+import {ref, onMounted , computed , watch} from 'vue'
+const todos = ref([])
+const name = ref('')
+
+const input_content = ref('')
+const input_category = ref(null)
+const addTodo = () => {}
+const todos_asc = computed(() => todos.value.sort((a, b) =>{
+  return b.createdAt - a.createdAt
+}))
+
+watch(name, ( newVal) => {
+  localStorage.setItem('name',newVal)
+})
 
 
+onMounted(() =>{
+  name.value = localStorage.getItem('name') || ''
+})
 </script>
 
 <template>
- Hello , World
+<main class = "app">
+<section class="greeting">
+  <h2 class="title">
+    Hello , <input type="text" placeholder="Name here" v-model="name"/>
+  </h2>
+</section>
+
+<section class="create-todo">
+  <h3> Create A todo</h3>
+
+  <form @submit.prevent="addTodo"></form>
+
+
+</section>
+
+</main>
+
 </template>
 
